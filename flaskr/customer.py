0cbd7841f663fec0b615fdb3eb637bc9e11e9401
orflarg <Cal.wooten95@gmail.com>
2021-03-27 17:08:36 -0500

    Update customer.py

--- flaskr/customer.py
+++ flaskr/customer.py
@@ -8,6 +8,14 @@ from flaskr.db import get_db
 
 bp = Blueprint('customer', __name__)
 
+def get_item(id):
+    item = get_db().execute(
+        #"" SELECT i.id, i.price, i.itemName, i.description, i.type
+            #FROM items i JOIN
+
+
+    )
+
 
 @bp.route('/')
 def index():
@@ -15,15 +23,16 @@ def index():
     table = db.execute('SELECT * FROM items').fetchall()
 
 
-    #Get orders
-    #order = db.execute(
-    # 'SELECT o.id, price, itemName, description'
-    # ' FROM orders o JOIN user u ON o.custID = u.id'
-    # ' ORDER BY created DESC'
-    #).fetchall()
-
     return render_template('customer/index.html', table=table)
 
-@bp.route('/checkout')
+
+
+@bp.route('/checkout/')
 def checkout():
+    return render_template('customer/checkout.html')
+
+@bp.route('/<int:id>/addItem', methods=('GET','POST'))
+@login_required
+def addItem(id):
+
     return render_template('customer/checkout.html')
